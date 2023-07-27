184c8947c9f5467f23dee31dbcaa75530bcc6edb
AlinaWang233 <jw146@rice.edu>
2023-07-27 21:14:11 +0800

    send request using caiyunai interpreter api

---
+++ simple-dict/simple-dict.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type DictRequest struct {
+	TransType string `json:"trans_type"`
+	Source    string `json:"source"`
+	UserId    string `json:"user_id"`
+}
+
+func main() {
+	client := &http.Client{}
+	// //network dict request paylaod
+	// var data = strings.NewReader(`{"trans_type":"en2zh","source":"good"}`)
+	request := DictRequest{TransType: "en2zh", Source: "good"} // struct
+	buf, err := json.Marshal(request)                          // parse the struct to json
+	if err != nil {
+		log.Fatal(err)
+	}
+	var data = bytes.NewReader(buf) //the output of json marshal is bytes
+	//create a new request(method, url, data stream)
+	req, err := http.NewRequest("POST", "https://api.interpreter.caiyunai.com/v1/dict", data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//copy the request as cURL and parse cURL to Go
+	//set the request header using the result after parsing
+	req.Header.Set("Connection", "keep-alive")
+	req.Header.Set("DNT", "1")
+	req.Header.Set("os-version", "")
+	req.Header.Set("sec-ch-ua-mobile", "?0")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36")
+	req.Header.Set("app-name", "xy")
+	req.Header.Set("Content-Type", "application/json;charset=UTF-8")
+	req.Header.Set("Accept", "application/json, text/plain, */*")
+	req.Header.Set("device-id", "")
+	req.Header.Set("os-type", "web")
+	req.Header.Set("X-Authorization", "token:qgemv4jr1y38jyq6vhvi")
+	req.Header.Set("Origin", "https://fanyi.caiyunapp.com")
+	req.Header.Set("Sec-Fetch-Site", "cross-site")
+	req.Header.Set("Sec-Fetch-Mode", "cors")
+	req.Header.Set("Sec-Fetch-Dest", "empty")
+	req.Header.Set("Referer", "https://fanyi.caiyunapp.com/")
+	req.Header.Set("Accept-Language", "zh-CN,zh;q=0.9")
+	req.Header.Set("Cookie", "_ym_uid=16456948721020430059; _ym_d=1645694872")
+	//send request
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//prevent data leak
+	defer resp.Body.Close()
+	bodyText, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", bodyText)
+}
